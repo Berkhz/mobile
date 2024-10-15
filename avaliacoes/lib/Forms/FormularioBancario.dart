63064809d359034be7b6df2d0e406cf9f1c08a41
Kauan Bertalha <kauan.hb2004@gmail.com>
2024-10-15 13:02:02 -0300

    Up

---
+++ avaliacoes/lib/Forms/FormularioBancario.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class FormularioConta extends StatefulWidget {
+  @override
+  _FormularioContaState createState() => _FormularioContaState();
+}
+
+class _FormularioContaState extends State<FormularioConta> {
+  final _nomeController = TextEditingController();
+  final _saldoController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Cadastro de Conta')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _nomeController,
+              decoration: InputDecoration(labelText: 'Nome da Conta'),
+            ),
+            TextField(
+              controller: _saldoController,
+              decoration: InputDecoration(labelText: 'Saldo Inicial'),
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Aqui você enviaria os dados para a API
+              },
+              child: Text('Cadastrar'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
